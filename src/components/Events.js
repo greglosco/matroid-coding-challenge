b3f2d788f1bf9bdd38e6e79e68b64f61e62b3044
Greg Losco <gtlosco@gmail.com>
2020-06-05 00:25:37 -0700

    finished prediction score filter method

--- src/components/Events.js
+++ src/components/Events.js
@@ -6,31 +6,37 @@ class Events extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            search: ""
+            labelSearch: "",
+            scoreSearch: "",
+            videoStreamSearch: "",
+            timestampSearch: ""
         }
     }
 
     handleOnChange = (event) => {
+        const { name, value} = event.target;
         this.setState({
-               search: event.target.value
+               [name]: value
         })
     }
     
-    // return event.videoStream.indexOf(this.state.search) !== -1;
-
     render() {
 
         let filteredEvents = this.props.events.filter(
             (event) => {
+                if (event.videoStream.indexOf(this.state.videoStreamSearch) !== -1) {
+                    return event.videoStream
+                } else {
                 const predictions = event.predictions;
                 for (const obj of predictions) {
                     const scores = obj.scores;
                 for (const score of scores) {
-                    if (score.label.indexOf(this.state.search) !== -1) {
+                    if ((score.label.indexOf(this.state.labelSearch) !== -1) && (score.score >= this.state.scoreSearch)) {
                         return score.label
-                    }
+                    } 
                 }
                 }
+            }
             }
         )
       
@@ -39,11 +45,11 @@ class Events extends Component {
         return (
             <div>
                 <div>
-                    <label>Filter By:</label>
-                    <input type="text" placeholder="prediction label" value={this.state.search} onChange={this.handleOnChange} />
-                    <input type="text" placeholder="prediction score" />
-                    <input type="text" placeholder="videostream" />
-                    <input type="text" placeholder="timestamp" />
+                    <label>Filter By: </label>
+                    <input type="text" name="labelSearch" placeholder="prediction label" value={this.state.labelSearch} onChange={this.handleOnChange} />
+                    <input type="text" name="scoreSearch" placeholder="prediction score" value={this.state.scoreSearch} onChange={this.handleOnChange} />
+                    <input type="text" name="videoStreamSearch" placeholder="videoStream" value={this.state.videoStreamSearch} onChange={this.handleOnChange} />
+                    <input type="text" name="timestampSearch" placeholder="timestamp" value={this.state.timestampSearch} onChange={this.handleOnChange} />
                 </div>
                 <div>
                     {renderedEvents}
